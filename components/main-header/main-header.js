b792e213c4e0bc8473780ab3e6837e401b960059
Anaro Karo <44401026+blendebox-karo@users.noreply.github.com>
2024-05-16 21:43:46 +0100

    Added foodies files

---
+++ components/main-header/main-header.js
@@ -0,0 +1,42 @@
+'use client'
+import Link from 'next/link'
+
+import logoimg from '@/assets/logo.png'
+
+import classes from './main-header.module.css'
+import Image from 'next/image'
+import MainHeaderBackground from './main-header-background'
+import { usePathname } from 'next/navigation'
+import NavLink from './nav-link'
+
+export default function MainHeader() {
+  const path = usePathname()
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image
+            src={logoimg.src}
+            width={40}
+            height={40}
+            alt="A plate with food on it"
+            priority
+          />
+          NextLevel Food
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <NavLink href="/meals">Browse Meals</NavLink>
+            </li>
+            <li>
+              <NavLink href="/community">Foodies Community</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  )
+}
